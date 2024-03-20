71f5ab4d0e07e0d8be703a5329d0854d4b93b259
ZSMDG <jon.ped.14@hotmail.com>
2024-03-20 14:00:32 +0000

    Add missing semi-colons to code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,11 @@
 // Wait for the DOM to finish loading before running game
 // Get the button elements and add event listeners to them
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     let buttons = document.getElementsByTagName("button");
 
     for (let button of buttons) {
-        button.addEventListener('click', function() {
+        button.addEventListener('click', function () {
             if (this.getAttribute("data-type") === "submit") {
                 checkAnswer();
             } else {
@@ -15,7 +15,7 @@ document.addEventListener("DOMContentLoaded", function() {
         })
     }
 
-    document.getElementById("answer-box").addEventListener('keydown', function(event) {
+    document.getElementById("answer-box").addEventListener('keydown', function (event) {
         if (event.key === "Enter") {
             checkAnswer();
         }
@@ -38,7 +38,7 @@ function runGame(gameType) {
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
 
-    let divisonNum = num1 * num2
+    let divisonNum = num1 * num2;
 
     if (gameType === "addition") {
         displayAdditionQuestion(num1, num2);
@@ -84,7 +84,7 @@ function calculateCorrectAnswer() {
     let operand1 = parseInt(document.getElementById('operand1').innerText);
     let operand2 = parseInt(document.getElementById('operand2').innerText);
     let operator = document.getElementById('operator').innerText;
-    
+
     if (operator === "+") {
         return [operand1 + operand2, "addition"];
     } else if (operator === "x") {
@@ -112,7 +112,7 @@ function incrementScore() {
  * Gets the current tally of incorrect answers from the DOM and increments it by 1
  */
 function incrementWrongAnswer() {
-    
+
     let oldScore = parseInt(document.getElementById("incorrect").innerText);
     document.getElementById("incorrect").innerText = ++oldScore;
 }
@@ -125,14 +125,14 @@ function displayAdditionQuestion(operand1, operand2) {
 }
 
 function displaySubtractQuestion(operand1, operand2) {
-    
+
     document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;
     document.getElementById('operand2').textContent = operand1 > operand2 ? operand2 : operand1;
     document.getElementById('operator').textContent = "-";
 }
 
 function displayMultiplyQuestion(operand1, operand2) {
-    
+
     document.getElementById('operand1').textContent = operand1;
     document.getElementById('operand2').textContent = operand2;
     document.getElementById('operator').textContent = "x";
@@ -141,6 +141,6 @@ function displayMultiplyQuestion(operand1, operand2) {
 function displayDivisionQuestion(divisionNum, operand2) {
 
     document.getElementById('operand1').textContent = divisionNum;
-    document.getElementById('operand2').textContent = operand2
+    document.getElementById('operand2').textContent = operand2;
     document.getElementById('operator').textContent = "/";
 }
